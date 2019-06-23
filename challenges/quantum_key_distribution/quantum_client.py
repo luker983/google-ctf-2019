c2a6495a4db7bed8af36d2de5558a0b5d0dbb789
Luke Rindels <raymond.rindels@student.nmt.edu>
2019-06-23 11:09:55 -0600

    Progress on QKD

--- challenges/quantum_key_distribution/quantum_client.py
+++ challenges/quantum_key_distribution/quantum_client.py
@@ -1,9 +1,11 @@
 import requests
 import random
 import json
+import subprocess
 
 LEN = 512
 URL = 'https://cryptoqkd.web.ctfcompetition.com/qkd/qubits'
+FLAG = 'U2FsdGVkX19OI2T2J9zJbjMrmI0YSTS+zJ7fnxu1YcGftgkeyVMMwa+NNMG6fGgjROM/hUvvUxUGhctU8fqH4titwti7HbwNMxFxfIR+lR4='
 
 axis = ['+', 'x']
 binary = [0, 1]
@@ -11,17 +13,34 @@ binary = [0, 1]
 data = {'basis': [], 'qubits': []}
 
 for x in range(LEN):
-    data['basis'].append(random.choice(axis))
+    axis = random.choice(axis)
     qubit = random.choice(binary)
-    if qubit == 1:
-        qubit = {'real': 1, 'imag': 0}
-    else:
-        qubit = {'real': 0, 'imag': 1}
+
+    if qubit == 0:
+        if axis == '+':
+            qubit = {'real': 1, 'imag': 0}
+        else: # axis = 'x'
+            qubit = {'real': 0.71, 'imag': 0.71}
+    else: # qubit = 1
+        if axis == '+':
+            qubit = {'real': 0, 'imag': 1}
+        else: # axis = 'x'
+            qubit = {'real': -0.71, 'imag': 0.71}        
 
     data['qubits'].append(qubit)
+    data['basis'].append(axis)
 
 #print(json.dumps(data, indent=4))
 
 r = requests.post(URL, json=data)
+#print(r.text)
+sat_response = r.json()
 
-print(r.text)
+shared_hex_key = sat_response['announcement']
+print('Shared Hex Key:', shared_hex_key)
+
+derive = '(echo "%s" > /tmp/plain.key; xxd -r -p /tmp/plain.key > /tmp/enc.key)'%(shared_hex_key)
+subprocess.call(derive, shell=True, stdout=subprocess.PIPE)
+
+decrypt = '(echo "%s" | openssl enc -d -aes-256-cbc -pbkdf2 -md sha1 -base64 --pass file:/tmp/enc.key > /tmp/flag.txt)'%(FLAG)
+subprocess.call(decrypt, shell=True, stdout=subprocess.PIPE)
